dda69ef96b0ddb09dcb33389a4c94aec5f658bfc
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-09-23 23:11:54 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import Notification from '../Notification';
 import { AppContainer } from './App.styled';
 
 export default function App() {
-  const [good, setGood] = useState();
+  const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const mount = useRef(true);
